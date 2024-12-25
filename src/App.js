8e4ec31f8fbf4a5f5e5f8a1f0b071421f745fd98
Alex Bibik <alexandr.bibik@gmail.com>
2024-12-25 23:31:11 +0200

    common classes setup

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Footer } from './components/Footer';
 
 function App() {
   return (
-    <div className='App'>
+    <div className='main'>
         <Header />
         <main>
         </main>
