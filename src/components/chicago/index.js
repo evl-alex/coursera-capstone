c1a8f844a93660aade5793cab9873fc8f00622d2
Alex Bibik <alexandr.bibik@gmail.com>
2025-01-19 12:44:48 +0200

    chicago section

--- src/components/chicago/index.js
+++ src/components/chicago/index.js
@@ -1,6 +1,22 @@
+import './style.css';
+
 const Chicago = () => (
-    <section>
-        <p>The component that describes the Little Lemon Chicago restaurant and gives a short backstory for it</p>
+    <section className='chicago section-container'>
+        <div className='chicago-text'>
+            <div className='heading'>
+                <h2 className='title'>Little Lemon</h2>
+                <h3 className='sub-title'>Chicago</h3>
+            </div>
+            <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis
+                enim velit mollit. Exercitation veniam consequat sunt nostrud amet. Amet minim mollit non deserunt
+                ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis
+                enim velit mollit.
+            </p>
+        </div>
+        <div className='chicago-images'>
+            <img className='chicago-images-top' src='/img/mario-and-adrian.jpg' alt='Mario and Adrian'/>
+            <img className='chicago-images-bottom' src='/img/restaurant.jpg' alt='restaurant'/>
+        </div>
     </section>
 );
 
